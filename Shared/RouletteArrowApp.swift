0079aad29303b5585acc039ea19bec4859ddd391
yuyakan <kanbe1365@icloud.com>
2022-03-02 22:41:23 +0900

    Initial Commit

---
+++ Shared/RouletteArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteArrowApp.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct RouletteArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
