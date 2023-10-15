ef8dbb2b7efd71f6c85ff0f54db2c0ce9b99ebb9
yuyakan <kanbe1365@icloud.com>
2023-10-16 01:47:23 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RouletteArrow (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RouletteArrow (iOS)
+	pod 'Google-Mobile-Ads-SDK'
+end
+
+target 'RouletteArrow (macOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RouletteArrow (macOS)
+
+end
---
+++ Shared/RouletteArrowApp.swift
@@ -0,0 +1,43 @@
+//
+//  RouletteArrowApp.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/03/02.
+//
+
+import SwiftUI
+import AppTrackingTransparency
+import UIKit
+import GoogleMobileAds
+
+// AppDelegateクラスを定義する
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Mobile Ads SDKを初期化する
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
+
+@main
+struct RouletteArrowApp: App {
+    init() {
+            if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+                //User has not indicated their choice for app tracking
+                //You may want to show a pop-up explaining why you are collecting their data
+                //Toggle any variables to do this here
+            } else {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    //Whether or not user has opted in initialize GADMobileAds here it will handle the rest
+                                                                
+                    GADMobileAds.sharedInstance().start(completionHandler: nil)
+                }
+            }
+        }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
